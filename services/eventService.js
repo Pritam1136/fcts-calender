382c7ed82fa8652f8ac2f1e6b578e83c2c3f5451
Pritam Roy <pritamroy1136@gmail.com>
2024-10-24 13:00:32 +0530

    More program on BST

--- services/eventService.js
+++ services/eventService.js
@@ -3,60 +3,70 @@ import { sendEmail } from "../utils/emailUtils.js";
 import { isEventTomorrow, isEventThisWeek, dates } from "../utils/dateUtils.js";
 
 function emailContent(name, eventType, startDate, _endDate) {
+  const formattedStartDate = new Date(startDate).toLocaleDateString("en-IN");
   const imageSrc =
     "https://media.licdn.com/dms/image/v2/C4D0BAQH6fJz1s57_eA/company-logo_200_200/company-logo_200_200/0/1630509348990/forwardcode_techstudio_logo?e=1736985600&v=beta&t=nlMSUu3V4zzN6zA9rlbOjdJE7IdnugYYZniJ09UTlNo";
+
   return `
   <body style="background-color: aliceblue; font-family: Arial, Helvetica, sans-serif; margin: 0; padding: 0;">
     <table align="center" cellpadding="0" cellspacing="0" style="max-width: 800px; width: 100%; margin: 3rem auto; background-color: aliceblue;">
+      <!-- Header Section -->
       <tr>
         <td align="center" style="padding: 1rem;">
-        <img src=${imageSrc ? imageSrc : null}
-        alt="logo" style="width: 90px; height: 90px; background-color: white; border-radius: 50%; margin-bottom: 1rem;">
-        <p style="font-size: 1.6rem; margin: 2px; text-align: center;">Hi <strong>${name},</strong></p>
-        <p style="font-size: 1.6rem; margin: 2px; text-align: center;">Event - ${eventType}</p>
+          <img src="${imageSrc}" alt="logo" style="width: 90px; height: 90px; background-color: white; border-radius: 50%; margin-bottom: 1rem;">
+          <p style="font-size: 1.6rem; margin: 2px; text-align: center;">Hi <strong>${name},</strong></p>
+          <p style="font-size: 1.6rem; margin: 2px; text-align: center;">${eventType} - Save the date</p>
         </td>
       </tr>
+      
+      <!-- Main Content Section -->
       <tr>
-        <td align="start" style="background-color: #ffffff; border-radius: 1rem; padding: 1rem 2rem; margin: 1.5rem 0;">
-          <p style="font-size: 13px; color: #5e5e5e; text-align: start; margin: 1rem 0 0;">
+        <td style="background-color: #ffffff; border-radius: 1rem; padding: 1rem 2rem; margin: 1.5rem 0;">
+          <p style="font-size: 13px; color: #5e5e5e; margin: 1rem 0;">Dear, ${name}</p>
+          <p style="font-size: 13px; color: #5e5e5e; margin: 1rem 0;">
             We are excited to inform you about an upcoming event: <strong>${eventType}</strong> scheduled for 
-            <strong>${startDate.slice(0, 10)}</strong>.
-          </p><br/>
-         <p style="font-size: 13px; color: #5e5e5e;">
-            This event will be a great opportunity to engage with your peers. 
-            We encourage you to mark your calendar and prepare for a meaningful experience.
+            <strong>${formattedStartDate}</strong>.
           </p>
-          <p style="font-size: 13px; color: #5e5e5e;">
-            If you have any questions or suggestions, feel free to reach out to us. We look forward to your participation!
-          </p>
+          <br/>
+          <p style="font-size: 13px; color: #5e5e5e;">We look forward to seeing you there!</p>
+          <p style="font-size: 13px; color: #5e5e5e;">In case of any queries or suggestions, please connect.</p>
         </td>
       </tr>
-      <tr style="padding: 1rem;" align="start">
-        <td>
-          <table width="100%" style="text-align: start; padding-top: 1rem">
-            <tr style="padding-top: 1rem">
-              <td style="padding-bottom: 1rem;" width="90">
-                <img src="https://media.licdn.com/dms/image/v2/C4D0BAQH6fJz1s57_eA/company-logo_200_200/company-logo_200_200/0/1630509348990/forwardcode_techstudio_logo?e=1736985600&v=beta&t=nlMSUu3V4zzN6zA9rlbOjdJE7IdnugYYZniJ09UTlNo" 
-                  alt="logo" style="width: 90px; height: 90px; background-color: white; margin-bottom: 1rem;">
+
+      <!-- Footer Section with HR Info -->
+      <tr>
+        <td style="padding: 1rem;">
+          <table width="100%" style="text-align: start;">
+            <tr>
+              <td width="90" style="padding-right: 1rem;">
+                <img src="${imageSrc}" alt="Forwardcode TechStudio" style="width: 90px; height: 90px; background-color: white; margin-bottom: 1rem;">
               </td>
-              <td style="padding-left: 1rem;">
+              <td>
                 <p style="font-size: 14px; font-weight: bold; color: #333;">TEAM HR</p>
                 <p style="font-size: 12px; color: #5e5e5e;">Forwardcode TechStudio</p>
                 <p style="font-size: 12px; color: #5e5e5e;">Jamshedpur, JH - 831018</p>
-                <p style="font-size: 12px; color: #5e5e5e;">hr@forwardcode.in</p>
-                <p style="font-size: 12px; color: #007bff;">https://forwardcode.in</p>
+                <p style="font-size: 12px;">hr@forwardcode.in</p>
+                <p style="font-size: 12px; color: black;">
+                  Check what's new: 
+                  <a href="https://forwardcode.in" style="color: #007bff;">https://forwardcode.in</a>
+                </p>
               </td>
             </tr>
           </table>
         </td>
       </tr>
-      <!-- Footer Section -->
-     <tr>
-       <td align="start" style="padding: 1rem;">
-        <p style="font-size: 8px; margin: 0;">PLEASE CONSIDER THE ENVIRONMENT BEFORE PRINTING THIS EMAIL.</p>
-        <p style="font-size: 8px; margin: 0;">THIS MESSAGE IS INTENDED ONLY FOR THE USE OF THE INDIVIDUAL OR ENTITY TO WHICH IT IS ADDRESSED AND MAY CONTAIN INFORMATION THAT IS PRIVILEGED, CONFIDENTIAL AND EXEMPT FROM DISCLOSURE UNDER APPLICABLE LAW. IF THE READER OF THIS MESSAGE IS NOT THE INTENDED RECIPIENT, OR THE EMPLOYEE OR AGENT RESPONSIBLE FOR DELIVERING THE MESSAGE TO THE INTENDED RECIPIENT, YOU ARE HEREBY NOTIFIED THAT ANY DISSEMINATION, DISTRIBUTION OR COPYING OF THIS COMMUNICATION IS STRICTLY PROHIBITED. IF YOU HAVE RECEIVED THIS COMMUNICATION IN ERROR, PLEASE NOTIFY US IMMEDIATELY BY TELEPHONE AND/OR RETURN E-MAIL. THANK YOU</p>
-       </td>
-     </tr>
+
+      <!-- Disclaimer Section -->
+      <tr>
+        <td style="padding: 1rem;">
+          <p style="font-size: 8px; margin: 0;">PLEASE CONSIDER THE ENVIRONMENT BEFORE PRINTING THIS EMAIL.</p>
+          <p style="font-size: 8px; margin: 0;">
+            THIS MESSAGE IS INTENDED ONLY FOR THE USE OF THE INDIVIDUAL OR ENTITY TO WHICH IT IS ADDRESSED AND MAY CONTAIN 
+            INFORMATION THAT IS PRIVILEGED, CONFIDENTIAL AND EXEMPT FROM DISCLOSURE UNDER APPLICABLE LAW.
+            IF YOU ARE NOT THE INTENDED RECIPIENT, YOU ARE HEREBY NOTIFIED THAT ANY DISSEMINATION OR COPYING IS STRICTLY PROHIBITED.
+          </p>
+        </td>
+      </tr>
     </table>
   </body>
   `;
@@ -102,12 +112,7 @@ export async function fetchDailyEventsAndSendEmails() {
           await sendEmail(
             userData.email,
             `Event Notification - ${name}`,
-            emailContent(
-              userData.name,
-              eventTypeData.name,
-              newStartDate,
-              newEndDate
-            )
+            emailContent(userData.name, eventTypeData.name, newStartDate)
           );
         }
       }
@@ -126,7 +131,7 @@ export async function fetchDailyEventsAndSendEmails() {
         await sendEmail(
           user.email,
           `Event Notification - ${name}`,
-          emailContent(user.name, eventTypeData.name, startDate, endDate)
+          emailContent(user.name, eventTypeData.name, startDate)
         );
       }
     }
